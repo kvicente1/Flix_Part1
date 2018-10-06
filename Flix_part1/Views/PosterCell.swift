8d8f55613ead8c9345fba5f57df4beaef427efee
Karina Vicente <karinavicente@Karinas-MacBook.local>
2018-10-05 18:07:35 -0700

    changes to flix part2

---
+++ Flix_part1/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix_part1
+//
+//  Created by Karina Vicente on 9/30/18.
+//  Copyright © 2018 Karina Vicente. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+  
+    @IBOutlet weak var posterImageView: UIImageView!
+}
